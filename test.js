374cc929845be33c8a1966fbfc62547d954331a1
rouvic <victor.rouanet@gmx.fr>
2020-12-24 13:44:45 +0100

    a bit of work

---
+++ test.js
@@ -0,0 +1,13 @@
+      const options = {
+  linkSelector:
+    'a[href^="' +
+    window.location.origin +
+    '"]:not([data-no-swup]), a[href^="/"]:not([data-no-swup]), a[href^="#"]:not([data-no-swup]), a[href^="./"]:not([data-no-swup])'
+};
+
+      const swup = new Swup(options);
+/**
+const swup = new Swup({
+  plugins: [new SwupScrollPlugin()]
+});
+**/
